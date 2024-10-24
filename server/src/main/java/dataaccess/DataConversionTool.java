83f4effb82e39160ebb1a88867db7ffbcb479bf3
KyleMak <khm45@byu.edu>
2024-10-23 20:15:15 -0600

    Made sql conversion tool helper

---
+++ server/src/main/java/dataaccess/DataConversionTool.java
@@ -0,0 +1,55 @@
+package dataaccess;
+
+import exception.ResponseException;
+import model.UserData;
+
+import javax.xml.crypto.Data;
+import java.sql.*;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
+
+public class DataConversionTool {
+    public class DataAccessUtility {
+        public static void executeUpdate(String statement, Object... params) throws ResponseException {
+            try (var conn = DatabaseManager.getConnection()) {
+                try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                    for (var i = 0; i < params.length; i++) {
+                        var param = params[i];
+                        switch (param) {
+                            case String p -> ps.setString(i + 1, p);
+                            case Integer p -> ps.setInt(i + 1, p);
+                            case UserData p -> ps.setString(i + 1, p.toString());
+                            case null -> ps.setNull(i + 1, NULL);
+                            default -> {
+                            }
+                        }
+                    }
+                    ps.executeUpdate();
+
+                    var rs = ps.getGeneratedKeys();
+                    if (rs.next()) {
+                        rs.getInt(1);
+                    }
+
+                }
+            } catch (DataAccessException | SQLException e) {
+                throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+            }
+        }
+
+        public static void configureDatabase(String[] createStatements) throws ResponseException, DataAccessException {
+            DatabaseManager.createDatabase();
+            try (var conn = DatabaseManager.getConnection()) {
+                for (var statement : createStatements) {
+                    try (var preparedStatement = conn.prepareStatement(statement)) {
+                        preparedStatement.executeUpdate();
+                    }
+                }
+            } catch (SQLException ex) {
+                throw new ResponseException(500, String.format("Unable to configure database: %s", ex.getMessage()));
+            }
+        }
+
+    }
+}
