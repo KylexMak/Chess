fe3ce9f3d6fe2c87322c452d37b120c1db728131
KyleMak <khm45@byu.edu>
2024-10-25 19:15:51 -0600

    Fixed nesting depth

--- server/src/main/java/dataaccess/DataConversionTool.java
+++ server/src/main/java/dataaccess/DataConversionTool.java
@@ -13,27 +13,30 @@ public class DataConversionTool {
     public static void executeUpdate(String statement, Object... params) throws ResponseException {
         try (var conn = DatabaseManager.getConnection()) {
             try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
-                for (var i = 0; i < params.length; i++) {
-                    var param = params[i];
-                    switch (param) {
-                        case String p -> ps.setString(i + 1, p);
-                        case Integer p -> ps.setInt(i + 1, p);
-                        case UserData p -> ps.setString(i + 1, p.toString());
-                        case null -> ps.setNull(i + 1, NULL);
-                        default -> {
-                        }
-                    }
-                }
-                ps.executeUpdate();
-
-                var rs = ps.getGeneratedKeys();
-                if (rs.next()) {
-                    rs.getInt(1);
-                }
-
+                setAndParseParams(params, ps);
             }
         } catch (DataAccessException | SQLException e) {
             throw new ResponseException(500, String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        }
+    }
+
+    private static void setAndParseParams(Object[] params, PreparedStatement ps) throws SQLException {
+        for (var i = 0; i < params.length; i++) {
+            var param = params[i];
+            switch (param) {
+                case String p -> ps.setString(i + 1, p);
+                case Integer p -> ps.setInt(i + 1, p);
+                case UserData p -> ps.setString(i + 1, p.toString());
+                case null -> ps.setNull(i + 1, NULL);
+                default -> {
+                }
+            }
+        }
+        ps.executeUpdate();
+
+        var rs = ps.getGeneratedKeys();
+        if (rs.next()) {
+            rs.getInt(1);
         }
     }
 
