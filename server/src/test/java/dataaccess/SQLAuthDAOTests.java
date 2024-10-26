56b7c68b3d44017693558169c9a032930554c16e
KyleMak <khm45@byu.edu>
2024-10-25 18:12:58 -0600

    Write Get tests for SQLAuthDAOTests

--- server/src/test/java/dataaccess/SQLAuthDAOTests.java
+++ server/src/test/java/dataaccess/SQLAuthDAOTests.java
@@ -6,31 +6,43 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import service.AuthService;
 
 
 public class SQLAuthDAOTests {
-    private AuthService authService;
+    private AuthDao authDB;
 
     @BeforeEach
     public void setUp() throws DataAccessException, ResponseException{
-        authService = new AuthService();
-        authService.createAuthData("Test");
+        authDB = new SQLAuthDAO();
+        authDB.createAuthData(new AuthData("test", "Test"));
     }
     @AfterEach
     public void cleanUp() throws ResponseException{
-        authService.clearAllAuthData();
+        authDB.clearAllAuthData();
     }
 
     @Test
     public void createAuthPositive() throws ResponseException{
-        AuthData auth = authService.createAuthData("PositiveTest");
-        Assertions.assertNotNull(authService.getAuthData(auth.authToken()));
+        authDB.createAuthData(new AuthData("positive", "PositiveTest"));
+        Assertions.assertNotNull(authDB.getAuthData("positive"));
     }
 
     @Test
     public void createAuthNegative() throws ResponseException{
-        ResponseException exception = Assertions.assertThrows(ResponseException.class, () -> authService.createAuthData("NegativeTest"));
-        //Assertions.assertEquals();
+        authDB.createAuthData(new AuthData("negative", "NegativeTest"));
+        ResponseException exception = Assertions.assertThrows(ResponseException.class, () -> authDB.createAuthData(new AuthData("negative", "NegativeTest")));
+        Assertions.assertNotNull(exception);
     }
+
+    @Test
+    public void getAuthPositive() throws ResponseException{
+        Assertions.assertNotNull(authDB.getAuthData("test"));
+    }
+
+    @Test
+    public void getAuthNegative() throws ResponseException{
+        Assertions.assertNull(authDB.getAuthData("random"));
+    }
+
+
 }
