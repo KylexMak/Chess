cb95b2978de1eee1c051b6f7113b2e14666ef89a
KyleMak <khm45@byu.edu>
2024-10-25 18:28:02 -0600

    Write Adding and get tests for userdb

---
+++ server/src/test/java/dataaccess/SQLUserDAOTests.java
@@ -0,0 +1,44 @@
+package dataaccess;
+
+import exception.ResponseException;
+import model.UserData;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SQLUserDAOTests {
+    private UserDAO userDb;
+
+    @BeforeEach
+    public void setUp() throws DataAccessException , ResponseException{
+        userDb = new SQLUserDAO();
+        userDb.addUser(new UserData("test", "testPassword", "test@test.com"));
+    }
+    @AfterEach
+    public void cleanUp() throws ResponseException{
+        userDb.clearUsers();
+    }
+
+    @Test
+    public void addingUserPositive() throws ResponseException{
+        userDb.addUser(new UserData("positive", "password", "positive@test.com"));
+        Assertions.assertNotNull(userDb.getUser("positive"));
+    }
+
+    @Test
+    public void addingUserNegative(){
+        ResponseException exception = Assertions.assertThrows(ResponseException.class, () -> userDb.addUser(new UserData(null, "password", "negative@test.com")));
+        Assertions.assertNotNull(exception);
+    }
+
+    @Test
+    public void getUserPositive() throws ResponseException{
+        Assertions.assertNotNull(userDb.getUser("test"));
+    }
+
+    @Test
+    public void getUserNegative() throws ResponseException{
+        Assertions.assertNull(userDb.getUser("random"));
+    }
+}
