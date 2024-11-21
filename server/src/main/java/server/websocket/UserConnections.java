630c9ef109ba551d968418821c9c6391b6b2302b
KyleMak <khm45@byu.edu>
2024-11-20 21:13:52 -0700

    user connections created

---
+++ server/src/main/java/server/websocket/UserConnections.java
@@ -0,0 +1,24 @@
+package server.websocket;
+
+import chess.ChessGame;
+
+import javax.websocket.Session;
+import java.io.IOException;
+
+public class UserConnections {
+    public String authToken;
+    public String username;
+    public ChessGame.TeamColor color;
+    public Session session;
+
+    public UserConnections(String authToken, String username, Session session, ChessGame.TeamColor color){
+        this.authToken = authToken;
+        this.username = username;
+        this.color = color;
+        this.session = session;
+    }
+
+    public void send(String message) throws IOException {
+        session.getBasicRemote().sendText(message);
+    }
+}
